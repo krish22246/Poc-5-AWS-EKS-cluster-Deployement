34f87e8b74f22929b2e820b731ab35c629ab1b96
krish22246 <seshasaikrishnac222@gmail.com>
2026-07-01 10:48:38 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,20 +1,3 @@
-terraform {
-  cloud {
-    organization = "YOUR_TFC_ORG"
-
-    workspaces {
-      name = "eks-poc-workspace"
-    }
-  }
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
 provider "aws" {
   region = var.region
 }
