24f7f52b6022963112c2c497c7b92eaafde68dfa
krish22246 <seshasaikrishnac222@gmail.com>
2026-07-01 10:50:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,6 @@
+##################################
+# VPC
+##################################
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "~> 5.0"
@@ -6,26 +9,45 @@ module "vpc" {
   cidr = "10.0.0.0/16"
 
   azs             = ["ap-south-1a", "ap-south-1b"]
-  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
-  private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.3.0/24", "10.0.4.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  tags = {
+    Environment = "poc"
+  }
 }
 
+##################################
+# EKS Cluster
+##################################
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 20.0"
 
-  cluster_name    = "eks-poc"
+  cluster_name    = var.cluster_name
   cluster_version = "1.29"
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
+  enable_irsa = true
+
+  cluster_endpoint_public_access = true
+
   eks_managed_node_groups = {
     default = {
       instance_types = ["t3.medium"]
-      desired_size   = 1
-      min_size       = 1
-      max_size       = 2
+
+      min_size     = 1
+      max_size     = 2
+      desired_size = 1
     }
+  }
+
+  tags = {
+    Environment = "poc"
   }
 }
